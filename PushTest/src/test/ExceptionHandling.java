0e54d4dd6e428aee627424059ebfe2d5987ba57b
anousackns <pa_akenoy18@hotmail.com>
2018-10-28 21:27:25 -0500

    added iinput

--- PushTest/src/test/ExceptionHandling.java
+++ PushTest/src/test/ExceptionHandling.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.InputMismatchException;
+
 public class ExceptionHandling {
 
 	public static void main(String[] args) {
@@ -7,14 +9,20 @@ public class ExceptionHandling {
 		System.out.println("Start");
 		
 		try {
-			int c=10/0;
+			int c=0/1;
 			
 			System.out.println("Result is "+c);
 			
 		} catch (ArithmeticException e) {
 
 			System.out.println("Something was wrong "+e.getMessage());
+			
+		} catch (InputMismatchException e) {
+			
+			System.out.println("Somewas wrong "+e.getMessage());
+			
 		}
+		
 		System.out.println("End");
 	}
 }
